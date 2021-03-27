0addfdb923639ce55c037e19547b192679579a41
Michael Groenewegen van der Weijden <michael@fragrant-chios.com>
2021-03-27 10:16:04 +0100

    refactored code + added params.yaml

--- create_dataset.py
+++ create_dataset.py
@@ -1,29 +1,19 @@
 import numpy as np
 import pandas as pd
 
-from sklearn.preprocessing import LabelEncoder
-
 
 column_names = ['buying', 'maint', 'doors', 'persons', 'lug_boot', 'safety', 'class/state']
 classes = ['unacc', 'acc', 'good', 'vgood']
 
+# read file from uci repository
+df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/car/car.data', 
+                header=None, 
+                names=column_names)
 
-# df = pd.read_csv('https://archive.ics.uci.edu/ml/machine-learning-databases/car/car.data', 
-#                 header=None, 
-#                 names=column_names)
-# df.to_csv('./assets/original_dataset/car_eval_raw.csv', index=False, mode='w')
-
-
-# load in the original dataset
-df = pd.read_csv('./assets/original_dataset/car_eval_raw.csv')
+# show info about dataset
+print(df.shape)
 print(df.head())
 
-print()
+# save raw dataset
+df.to_csv('./assets/original_dataset/car_eval_raw.csv', index=False, mode='w')
 
-# encode categorical values
-label_encoder = LabelEncoder()
-for column, _ in df.iteritems():
-    df[column] = label_encoder.fit_transform(np.array(df[column]))
-print(df.head())
-
-df.to_csv('./assets/data/car_eval_encoded.csv', index=False, mode='w')
--- create_features.py
+++ create_features.py
@@ -1,21 +1,35 @@
 import numpy as np
 import pandas as pd
+
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 
-
-df = pd.read_csv('./assets/data/car_eval_encoded.csv')
+# load in raw dataset from local dir
+df = pd.read_csv('./assets/original_dataset/car_eval_raw.csv')
 column_names = list(df.columns)
 
+
+# encode categorical values
+label_encoder = LabelEncoder()
+for column, _ in df.iteritems():
+    df[column] = label_encoder.fit_transform(np.array(df[column]))
 print(df.head())
 
+# save encoded values
+df.to_csv('./assets/data/car_eval_encoded.csv', index=False, mode='w')
+
+
+# split features and labels
 y = np.array(df.pop('class/state'))
 X = np.array(df)
 
+# split into train and test sub datasets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-print(column_names[:-1])
-print(column_names[-1])
+print(f'features: {column_names[:-1]}')
+print(f'label: {column_names[-1]}')
 
+# create dataframes from numpy arrays
 X_train_df = pd.DataFrame(X_train, columns=column_names[:-1])
 X_test_df = pd.DataFrame(X_test, columns=column_names[:-1])
 
@@ -23,6 +37,7 @@ y_train_df = pd.DataFrame(y_train, columns=[column_names[-1]])
 y_test_df = pd.DataFrame(y_test, columns=[column_names[-1]])
 
 
+# save features and labels
 X_train_df.to_csv('./assets/features/X_train.csv', index=False, mode='w')
 X_test_df.to_csv('./assets/features/X_test.csv', index=False, mode='w')
 
